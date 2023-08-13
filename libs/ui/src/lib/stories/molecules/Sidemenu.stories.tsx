18e6d48a3fdc0d2ed640b1aa118edfed3300ffd5
Benjamin Raffetseder <hello@benjamin-raffetseder.com>
2023-08-13 23:37:30 +0200

    + sidemenu story

---
+++ libs/ui/src/lib/stories/molecules/Sidemenu.stories.tsx
@@ -0,0 +1,14 @@
+'use client'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Sidemenu } from '../../client/molecules/Sidemenu'
+
+const meta: Meta<typeof Sidemenu> = {
+  component: Sidemenu,
+}
+
+export default meta
+type Story = StoryObj<typeof Sidemenu>
+
+export const Primary: Story = {
+
+}
