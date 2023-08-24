cbfc4e96e9164b243d7626c6e84a5af72d71e15b
Benjamin Raffetseder <hello@benjamin-raffetseder.com>
2023-08-24 17:40:05 +0200

    ~ deps update

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,14 +1,15 @@
+'use client'
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 export * from './ThemeProvider'
 
 // Atoms
-export * from './lib/client/atoms/Button'
-export * from './lib/client/atoms/ThemeSwitch/'
 export * from './lib/client/atoms/Box'
-export * from './lib/client/atoms/NextLink'
+export * from './lib/client/atoms/Button'
 export * from './lib/client/atoms/Container'
-export * from './lib/client/atoms/Text'
 export * from './lib/client/atoms/Grid'
+export * from './lib/client/atoms/NextLink'
+export * from './lib/client/atoms/Text'
+export * from './lib/client/atoms/ThemeSwitch/'
 
 // Molecules
 export * from './lib/client/molecules/Navigation'
